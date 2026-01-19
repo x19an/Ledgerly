af2f53e62284db1920f4981615c28edac0ff8709
x19an <xxaneexaxx564@gmail.com>
2026-01-19 16:31:53 +0500

    .

--- run.bat
+++
@@ -1,8 +0,0 @@
-
-@echo off
-echo Starting Ledgerly Backend...
-start /b node server.js
-echo Starting Ledgerly Frontend...
-start /b npm run dev
-timeout /t 5 /nobreak > nul
-start http://localhost:3000/
